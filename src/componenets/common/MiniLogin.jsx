46de5ba71a16f133f3b718953b2aed6faa534842
ericagong <48196721+ericagong@users.noreply.github.com>
2022-08-20 16:14:12 +0900

    minilogin

---
+++ src/componenets/common/MiniLogin.jsx
@@ -0,0 +1,13 @@
+import Login from "../auth/Login";
+
+// TODO change to mini size!
+const MiniLogin = (props) => {
+  return (
+    <>
+      MiniLogin
+      <Login />
+    </>
+  );
+};
+
+export default MiniLogin;
--- src/componenets/post/HomeLayout.jsx
+++ src/componenets/post/HomeLayout.jsx
@@ -1,11 +1,12 @@
+import { useSelector } from "react-redux";
+
 import MiniUser from "../common/MiniUser";
+import MiniLogin from "../common/MiniLogin";
 
 const HomeLayout = (props) => {
-  return (
-    <>
-      <MiniUser />
-    </>
-  );
+  const isLogin = useSelector((state) => state.user.isLogin);
+
+  return <>{isLogin ? <MiniUser /> : <MiniLogin />}</>;
 };
 
 export default HomeLayout;
