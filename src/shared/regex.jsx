423986a35e1447479f676dc0bc70ae3e73295ed1
ericagong <48196721+ericagong@users.noreply.github.com>
2022-08-20 11:50:35 +0900

    regExp

--- src/shared/regex.jsx
+++ src/shared/regex.jsx
@@ -1,18 +1,19 @@
-export const idCheck = (id) => {
+export const usernameCheck = (id) => {
   let regExp = /^[0-9a-zA-Z]/;
   // 대문자 포함
   return regExp.test(id);
 };
 
-export const nickCheck = (nick) => {
-  let regExp = /^[0-9a-zA-Zㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+// export const nickCheck = (nick) => {
+//   let regExp = /^[0-9a-zA-Zㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
 
-  return regExp.test(nick);
-};
+//   return regExp.test(nick);
+// };
 
+// TODO regex 한번 체크해 코드 이해하기
 export const emailCheck = (email) => {
-  let _reg =
+  let regExp =
     /^[0-9a-zA-Z]([-_.0-9a-zA-Z])*@[0-9a-zA-Z]([-_.0-9a-zA-z])*.([a-zA-Z])*/;
 
-  return _reg.test(email);
+  return regExp.test(email);
 };
