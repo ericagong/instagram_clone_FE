9f29d354979bf1f61bcec4e01ae27e041ca04c4b
ericagong <48196721+ericagong@users.noreply.github.com>
2022-08-20 11:52:08 +0900

    useInput(hook)

--- src/shared/useInput.jsx
+++ src/shared/useInput.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const useInput = (initialValue, validator) => {
-  const [value, setValue] = React.useState(initialValue);
+  const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
     const {
       target: { value },
     } = event;
+
     console.log(value);
+
     let willUpdate = true;
     if (typeof validator === "function") {
       willUpdate = validator(value);
     }
+
     if (willUpdate) {
       setValue(value);
     }
