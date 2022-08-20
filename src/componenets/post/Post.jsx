0153da1fe151a09d514fcf48fc290b1471822a14
ericagong <48196721+ericagong@users.noreply.github.com>
2022-08-21 00:07:50 +0900

    feature/follow_unfollow

--- src/componenets/post/Post.jsx
+++ src/componenets/post/Post.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 import { apis } from "../../shared/axios";
 import RESP from "../../server/response";
 import ImgView from "./ImgView";
 import UserProfile from "../../elements/UserProfile";
-import Like from "../../elements/Like";
+import LikeBtn from "../../elements/LikeBtn";
 
 // edit delete
 // TODO content hashtag Link
-// TODO Like, Comment, More component 분리!
+// TODO Comment, More component (categories) 분리!
 const Post = ({
   id,
   username,
@@ -23,9 +24,13 @@ const Post = ({
   numcomments,
   numlikes,
 }) => {
+  const isLogin = useSelector((state) => state.user.isLogin);
+
   const [hasImage, setHasImage] = useState(imageUrls.length !== 0);
   const [showComment, setShowComment] = useState(false);
   const [showMore, setShowMore] = useState(false);
+
+  const [inEdit, setInEdit] = useState(false);
 
   const [isFollowing, setIsFollowing] = useState(isfollowing);
 
@@ -34,14 +39,66 @@ const Post = ({
   };
 
   const toggleMore = () => {
+    if (!isLogin) {
+      alert("Sorry. Only logged in user can see more.");
+      return;
+    }
+
     setShowMore((prev) => !prev);
   };
 
-  const clickFollow = () => {};
+  // 서버에 요청만 보내고, 리렌더링 하지 않고 토글처리만 하기!
+  const toggleFollow = () => {
+    if (!isFollowing) {
+      // const resp = await apis.follow_user(username);
+      // const { result, status: { message } } = resp.data;
 
-  const clickUnfollow = () => {};
+      // success
+      const {
+        result,
+        status: { message },
+      } = RESP.FOLLOW.FOLLOW_SUCCESS;
 
-  const clickEdit = () => {};
+      // fail
+      // const { result, status: { message } } = RESP.FOLLOW.FOLLOW_FAIL;
+
+      // TODO Change to modal!
+      // 색상 다르게 처리하기
+      if (!result) {
+        alert(message);
+        return;
+      }
+
+      alert(message);
+
+      setIsFollowing(true);
+    } else {
+      // const resp = await apis.unfollow_user(username);
+      // const { result, status: { message } } = resp.data;
+
+      // success
+      const {
+        result,
+        status: { message },
+      } = RESP.FOLLOW.UNFOLLOW_SUCCESS;
+
+      // fail
+      // const { result, status: { message } } = RESP.FOLLOW.UNFOLLOW_FAIL;
+
+      // TODO Change to modal!
+      // 색상 다르게 처리하기
+      if (!result) {
+        alert(message);
+        return;
+      }
+
+      alert(message);
+
+      setIsFollowing(false);
+    }
+  };
+
+  const toggleEdit = () => {};
 
   const clickDelete = () => {};
 
@@ -61,7 +118,7 @@ const Post = ({
         </div>
         <div>
           <div>
-            <Like isliked={isliked} />
+            <LikeBtn isliked={isliked} isLogin={isLogin} />
             <div>{numlikes}</div>
           </div>
           <div>
@@ -88,19 +145,19 @@ const Post = ({
             )}
             {showMore ? (
               <div>
-                {!ismine && !isfollowing ? (
-                  <button type='button' onClick={clickFollow}>
+                {!ismine && !isFollowing ? (
+                  <button type='button' onClick={toggleFollow}>
                     Follow this user
                   </button>
                 ) : null}
-                {!ismine && isfollowing ? (
-                  <button type='button' onClick={clickUnfollow}>
+                {!ismine && isFollowing ? (
+                  <button type='button' onClick={toggleFollow}>
                     Unfollow this user
                   </button>
                 ) : null}
                 {ismine ? (
                   <div>
-                    <button type='button' onClick={clickEdit}>
+                    <button type='button' onClick={toggleEdit}>
                       Edit this post
                     </button>
                     <button type='button' onClick={clickDelete}>
--- src/elements/LikeBtn.jsx
+++ src/elements/LikeBtn.jsx
@@ -3,11 +3,16 @@ import { useState } from "react";
 import { apis } from "../shared/axios";
 import RESP from "../server/response";
 
-const Like = ({ isliked }) => {
+const LikeBtn = ({ isliked, isLogin }) => {
   const [isLiked, setIsLiked] = useState(isliked);
 
   // 서버에 요청만 보내고, 리렌더링 하지 않고 토글처리만 하기!
   const toggleLike = async () => {
+    if (!isLogin) {
+      alert("Sorry. Only logged in user can like post.");
+      return;
+    }
+
     if (!isLiked) {
       // const resp = await apis.like_post(id);
       // const { result, status: { message } } = resp.data;
@@ -68,4 +73,4 @@ const Like = ({ isliked }) => {
   );
 };
 
-export default Like;
+export default LikeBtn;
