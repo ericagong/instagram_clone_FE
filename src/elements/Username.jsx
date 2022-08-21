2e2f35dad0429cfbae0cb166f532ab877f42e4cc
ericagong <48196721+ericagong@users.noreply.github.com>
2022-08-22 08:35:48 +0900

    bug/realative_path

--- src/elements/Username.jsx
+++ src/elements/Username.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 
+// TODO 상대경로 사용해봤음. 트러블슈팅.
 const Username = ({ isme, username }) => {
   return (
     <>
-      <Link to={!isme ? `profile/${username}` : "profile/"}>
+      <Link to={!isme ? `/profile/${username}` : "/profile/"}>
         <div>{username}</div>
       </Link>
     </>
