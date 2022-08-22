7012327e162085679629553c668cb3f7a73b7343
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-22 17:40:07 +0900

    feat/signup

--- src/componenets/auth/Signup.jsx
+++ src/componenets/auth/Signup.jsx
@@ -1,19 +1,78 @@
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-
 import { LOGIN_PATH } from "../../shared/paths";
 import { apis } from "../../shared/axios";
 import RESP from "../../server/response";
+import { useFormik } from "formik";
 import { emailCheck, usernameCheck, pwCheck } from "../../shared/regex";
+import * as Yup from "yup";
 
 const Signup = (props) => {
-  const {
-    register,
-    watch,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ mode: "onChange" });
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email("You should write in proper email format.")
+      .required("Required"),
+  });
+
+  const validate = (values) => {
+    const errors = {};
+
+    if (!emailCheck(values.email)) {
+      errors.email = "You should write in proper email format.";
+    }
+
+    if (!usernameCheck(values.username)) {
+      errors.username =
+        "Username should consists of english letters or numbers.";
+    }
+
+    if (!pwCheck(values.password)) {
+      errors.password =
+        "Password should be mixture of english letters, numbers and special characters.(@$!%*#?&)";
+    }
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      username: "",
+      password: "",
+      passwordCheck: "",
+    },
+    onSubmit: ({ email, username, password }) => {
+      console.log(email, username, password);
+
+      // const {
+      //   data: {
+      //     result,
+      //     status: { message },
+      //   },
+      // } = await apis.signup(email, username, password);
+
+      // success
+      const {
+        result,
+        status: { message },
+      } = RESP.AUTH.SIGN_UP_SUCCESS;
+
+      // fail
+      // const {
+      //   result,
+      //   status: { message },
+      // } = RESP.AUTH.SIGN_UP_FAIL;
+
+      // TODO 하단 모달로 처리
+      if (!result) {
+        alert(message);
+        return;
+      }
+
+      navigate(LOGIN_PATH);
+    },
+    validate,
+  });
 
   const [show, setShow] = useState(false);
 
@@ -23,131 +82,73 @@ const Signup = (props) => {
     setShow((prev) => !prev);
   };
 
-  const onSubmitHandler = async ({ email, username, password }) => {
-    // const {
-    //   data: {
-    //     result,
-    //     status: { message },
-    //   },
-    // } = await apis.signup(email, username, password);
-
-    // success
-    const {
-      result,
-      status: { message },
-    } = RESP.AUTH.SIGN_UP_SUCCESS;
-
-    // fail
-    // const {
-    //   result,
-    //   status: { message },
-    // } = RESP.AUTH.SIGN_UP_FAIL;
-
-    // TODO 하단 모달로 처리
-    if (!result) {
-      alert(message);
-      return;
-    }
-
-    navigate(LOGIN_PATH);
-  };
-
   return (
     <>
       <Link to={LOGIN_PATH}>
         <div>Go Sign in</div>
       </Link>
-      <form onSubmit={handleSubmit(onSubmitHandler)}>
+      <form onSubmit={formik.handleSubmit}>
         <div>
-          <label htmlFor='email'>email</label>
+          <label htmlFor="email">email</label>
           <input
-            type='text'
-            id='email'
-            {...register("email", {
-              required: "You should write email.",
-              validate: {
-                type: (value) =>
-                  emailCheck(value) ||
-                  "You should write in proper email format.",
-              },
-            })}
+            type="text"
+            id="email"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
           />
-          {errors.email ? <div>{errors.email.message}</div> : null}
+          {formik.errors.email && (
+            <span role="alert">{formik.errors.email}</span>
+          )}
         </div>
         <div>
-          <label htmlFor='username'>username</label>
+          <label htmlFor="username">username</label>
           <div>Username should consists of english letters or number.</div>
           <input
-            type='text'
-            id='username'
-            {...register("username", {
-              required: "You should write username.",
-              validate: {
-                type: (value) =>
-                  usernameCheck(value) ||
-                  "Username should consists of english letters or numbers.",
-              },
-              minLength: {
-                value: 3,
-                message: "Username should be longer than 2 letters.",
-              },
-              maxLength: {
-                value: 10,
-                message: "Username should be longer than 2 letters.",
-              },
-            })}
+            type="text"
+            id="username"
+            name="username"
+            value={formik.values.username}
+            onChange={formik.handleChange}
           />
-          {errors.username ? <div>{errors.username.message}</div> : null}
+          {formik.errors.username && (
+            <span role="alert">{formik.errors.username}</span>
+          )}
         </div>
         <div>
-          <label htmlFor='password'>password</label>
+          <label htmlFor="password">password</label>
           <div>
             Password should be length of 8 to 15, mixture of english letters,
             number and following special characters(@$!%*#?&).
           </div>
           <input
             type={!show ? "password" : "text"}
-            id='password'
-            {...register("password", {
-              required: "You should write password",
-              minLength: {
-                value: 8,
-                message: "Password shoule be longer than 7 letters.",
-              },
-              maxLength: {
-                value: 15,
-                message: "Password shoule be shorter than 16 letters.",
-              },
-              validate: {
-                check: (value) =>
-                  pwCheck(value) ||
-                  "Password should be mixture of english letters, numbers and special characters.(@$!%*#?&)",
-              },
-            })}
+            id="password"
+            name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
           />
-          <button type='button' onClick={onShowHandler}>
+          <button type="button" onClick={onShowHandler}>
             show
           </button>
-          {errors.password ? <div>{errors.password.message}</div> : null}
+          {formik.errors.password && (
+            <span role="alert">{formik.errors.password}</span>
+          )}
         </div>
         <div>
-          <label htmlFor='passwordCheck'>password check</label>
+          <label htmlFor="passwordCheck">password check</label>
           <input
             type={!show ? "password" : "text"}
-            id='passwordCheck'
-            {...register("passwordCheck", {
-              required: "You should check password again",
-              validate: {
-                check: (value) =>
-                  watch("password") === value || "Password not matched!",
-              },
-            })}
+            id="passwordCheck"
+            name="passwordCheck"
+            value={formik.values.passwordCheck}
+            onChange={formik.handleChange}
           />
-          {errors.passwordCheck ? (
-            <div>{errors.passwordCheck.message}</div>
-          ) : null}
+          {formik.errors.passwordCheck && (
+            <span role="alert">{formik.errors.passwordCheck}</span>
+          )}
         </div>
-        <button type='submit'>Sign up</button>
+        <button type="submit">Sign up</button>
       </form>
     </>
   );
